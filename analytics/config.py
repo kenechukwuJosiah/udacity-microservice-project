1dea323ef42670e497c42c2ef8309ed1bd803897
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2023-12-31 01:24:58 +0100

    update: hard coded databse parameters

--- analytics/config.py
+++ analytics/config.py
@@ -4,9 +4,9 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
-db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_username = "postgres"
+db_password = "53kUHnyQvS"
+db_host = "postgres-db-postgresql.default.svc.cluster.local"
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
